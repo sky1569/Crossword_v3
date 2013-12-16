978f426c9173efab72e1256187b9fca866196657
qicaoyan111 <15210830240@139.com>
2013-12-16 09:37:02 +0800

    turn back

--- src/com/crossword/utils/Module.java
+++ src/com/crossword/utils/Module.java
@@ -20,8 +20,14 @@ public class Module {
 	 private LinkedList<Word> entries  =  new LinkedList<Word>();
 	 private JsonUtil jsonUtil;
 	 private DBManager dbManager;
+	 private int width;
+	 private int height;
+	private String[][]		area;			// Tableau repr茅sentant les lettres du joueur
+	private String[][] 		displayArea;
+	private String[][] 		correctionArea; // Tableau repr茅sentant les lettres correctes
 	 public void parseGrid(Context context,String filename){
 		 this.grid = new Grid();
+		 
 		 jsonUtil = new JsonUtil(context);
 		 dbManager = new DBManager(context);
 		// String jsonData = jsonUtil.readJsonDataFromFile(filename);
@@ -29,6 +35,11 @@ public class Module {
 		 this.grid =  jsonUtil.parseGridJson(jsonData);
 		//解析完，将Json数据加入数据库中
 		 dbManager.add(this.grid);
+		 this.width = grid.getWidth();
+		 this.height = grid.getHeight();
+		 this.area = new String[this.height][this.width];
+	     this.correctionArea = new String[this.height][this.width];
+	     this.displayArea = new String[this.height][this.width];
 	 }
 	 
 	
@@ -42,7 +53,7 @@ public class Module {
 	    	return  currentWords.equalsIgnoreCase(correctWords)==true ?true:false;
 	    }
 	  
-	  public Word getWord(int x, int y, boolean horizontal)
+	  public Word getCorrectWord(int x, int y, boolean horizontal)
 	    {
 	        Word horizontalWord = null;
 	        Word verticalWord = null;
@@ -65,16 +76,57 @@ public class Module {
 		    	return (verticalWord != null) ? verticalWord : horizontalWord;
 	    }
 	  
-	  public void disTip()
-	  {
+		  public void disTip()//错误提示
+		  {
+			  
+		  }
 		  
-	  }
-	
+		  public void replay() //记得调用重绘
+		  {
+			  for(int i = 0;i < this.width;i++)
+				  for(int j = 0;j < this.height;j++)
+				  {
+					  if(this.isBlock(i,j))
+						  continue;
+					  else {
+						  		this.setDisValue(i, j, Crossword.BLANK);
+						  		this.setValue(i, j, Crossword.BLANK);
+				  }
+				  
+			
+			  }
+		  }
+		 public boolean isBlock(int x, int y)
+		 {
+				return (this.area[y][x] == null);
+		 }
+	   	public boolean isChinese(int x,int y)
+		{
+				
+			return  this.displayArea[y][x].getBytes().length == this.displayArea[y][x].length()?false:true;
+		}
+			
+		public void setValue(int x, int y, String value) {
+			if (this.area[y][x] != null&&!this.isChinese(x,y))
+			
+				{
+					this.area[y][x] = value.toUpperCase();
+					//	System.out.println(this.area[y][x]);
+				}
+			}
+		public void setDisValue(int x, int y, String value) {
+			if (this.area[y][x] != null&&!this.isChinese(x,y))
+			
+				{
+				this.displayArea[y][x] = value.toUpperCase();
+				System.out.println(this.displayArea[y][x]);
+				}
+		}
 	  
 		//保存Grid信息，先写入JSON，再写入数据库
 		public void save(GameGridAdapter gridAdapter,Grid grid){
 			for(Word entry:grid.getEntries()){
-				String word = gridAdapter.getWord(entry.getX(), entry.getY(),entry.getLength(),entry.getHoriz());
+				String word = this.getWord(entry.getX(), entry.getY(),entry.getLength(),entry.getHoriz());
 				entry.setTmp(word);			                                                         
 			}
 			JSONObject jObj = jsonUtil.writeToJson(grid);
@@ -91,5 +143,163 @@ public class Module {
 			this.grid = grid;
 			return grid;
 			
+		}
+		
+		public void initentries()
+		{
+			
+			for (Word entry: entries)
+		    {
+		    	String tmp = entry.getTmp();
+		    	String text = entry.getCap();
+		    	boolean horizontal = entry.getHoriz();
+		    	int x = entry.getX();
+		    	int y = entry.getY();
+		    	
+		    	for (int i = 0 ; i < entry.getLength(); i++) 
+		    	{
+		    		if (horizontal)
+		    		{
+		    			if (y >= 0 && y < this.height && x+i >= 0 && x+i < this.width)
+		    			{
+		    				this.area[y][x+i] = String.valueOf(tmp.charAt(i)).equals(Crossword.TMPSIGN)?Crossword.BLANK:String.valueOf(tmp.charAt(i));
+		    				this.displayArea[y][x+i] = String.valueOf(tmp.charAt(i)).equals(Crossword.TMPSIGN)?Crossword.BLANK:String.valueOf(tmp.charAt(i));
+		    				this.correctionArea[y][x+i] = String.valueOf(text.charAt(i));
+		    			}
+		    		}
+		    		else
+		    		{
+		    			if (y+i >= 0 && y+i < this.height && x >= 0 && x < this.width)
+		    			{
+		    				this.area[y+i][x] =  String.valueOf(tmp.charAt(i)).equals(Crossword.TMPSIGN)?Crossword.BLANK:String.valueOf(tmp.charAt(i));
+		    				this.displayArea[y+i][x] = String.valueOf(tmp.charAt(i)).equals(Crossword.TMPSIGN)?Crossword.BLANK:String.valueOf(tmp.charAt(i));
+		    				this.correctionArea[y+i][x] = String.valueOf(text.charAt(i));
+		    			}
+		    		}
+		    	}
+		    	
+		   }
+			this.toChinese();
+		}
+			
+			public void toChinese()
+			{
+				for(int currentX=0; currentX<this.width;currentX++)
+			    	for(int currentY=0;currentY<this.height;currentY++)
+			    		{
+			    		   if(this.isBlock(currentX,currentY))
+			    			  continue;
+			    		   if(this.area[currentY][currentX]==null)
+			    			   continue;
+
+			    		   Word currentWord = this.getCorrectWord(currentX,currentY,true);
+			     			   if(this.isCorrect(this.getCorrectWord(currentWord.getX(), currentWord.getY(), currentWord.getHoriz()).getCap(),this.getWord(currentWord.getX(),currentWord.getY(),currentWord.getLength(), currentWord.getHoriz())))
+			       		    	{
+			       				  for(int l = 0; l < currentWord.getLength(); l++)
+			       				  {
+			       					if(currentWord.getHoriz())  this.setDisValue(currentWord.getX()+l, currentWord.getY(),currentWord.getAns(l));
+			       					       						
+			            		    if(!currentWord.getHoriz()) this.setDisValue(currentWord.getX(), currentWord.getY()+l,currentWord.getAns(l));  
+			       		            		   
+			       				  }
+			       		    	}
+			    		    if(this.isCross(currentX,currentY))
+			        		  {
+			    			   if(this.isCorrect(this.getCorrectWord(currentX, currentY, !currentWord.getHoriz()).getCap(),
+			    					   this.getWord(this.getCorrectWord(currentX, currentY, !currentWord.getHoriz()).getX(),this.getCorrectWord(currentX, currentY, !currentWord.getHoriz()).getY(), 
+			    							   this.getCorrectWord(currentX, currentY, !currentWord.getHoriz()).getLength(), 
+			    							   this.getCorrectWord(currentX, currentY, !currentWord.getHoriz()).getHoriz())))
+				   		    	{
+				   				  for(int l = 0; l < this.getCorrectWord(currentX, currentY, !currentWord.getHoriz()).getLength(); l++)
+				   				    {
+				   						if(!currentWord.getHoriz()) 
+				   						{
+
+				   							this.setDisValue(this.getCorrectWord(currentX, currentY, !currentWord.getHoriz()).getX()+l,
+				   									this.getCorrectWord(currentX, currentY, !currentWord.getHoriz()).getY(),this.getCorrectWord(currentX, currentY, !currentWord.getHoriz()).getAns(l));
+				   						}
+				   						if(currentWord.getHoriz())
+				   		            	{
+				   							this.setDisValue(this.getCorrectWord(currentX, currentY, !currentWord.getHoriz()).getX(),
+				   									this.getCorrectWord(currentX, currentY, !currentWord.getHoriz()).getY()+l,this.getCorrectWord(currentX, currentY, !currentWord.getHoriz()).getAns(l));  
+
+				   		            	}
+				   		            }
+				   		         
+				   			     }   
+			    	       }
+			    		}  		
+			}
+				
+					
+			
+		
+		
+		public String getareaValue(int x,int y)
+		
+		{
+			System.out.println("fanhui le zhege zhi"+this.area[y][x]);
+			return this.area[y][x];
+		}
+		
+		public String getdisplayAreaValue(int x,int y)
+		
+		{
+			System.out.println("fanhui le zhege zhi"+this.area[y][x]);
+			return this.displayArea[y][x];
+		}
+		
+		public String getcorrectionAreaValue(int x,int y)
+		
+		{
+			System.out.println("fanhui le zhege zhi"+this.area[y][x]);
+			return this.correctionArea[y][x];
+		}
+		
+		public boolean isCross(int x,int y){
+			boolean c = false;
+			String lD,rD,tD,bD;
+			
+	        lD = x - 1 < 0 ? null:this.area[y][x - 1];
+	        rD = x + 1 >= this.width?null:this.area[y][x + 1];
+	        tD = y - 1 < 0 ? null:this.area[y - 1][x];
+	        bD = y + 1 >= this.height?null:this.area[y + 1][x];
+			
+	        if((lD == null && rD == null) || (tD == null && bD == null)){
+	        	c = false;
+	        }else{
+	        	c = true;
+	        }
+			return c;
+		}
+		
+		public String getWord(int x, int y, int length, boolean isHorizontal) {
+	    	StringBuffer word = new StringBuffer();
+	    	for (int i = 0; i < length; i++) {
+	    		if (isHorizontal) {
+	    			if (y < this.height && x+i < this.width)
+	    				word.append(this.area[y][x+i].equals(Crossword.BLANK)?Crossword.TMPSIGN:this.area[y][x+i]);
+	    		}
+	    		else {
+	    			if (y+i < this.height && x < this.width)
+	    				word.append(this.area[y+i][x].equals(Crossword.BLANK)?Crossword.TMPSIGN:this.area[y+i][x]);
+	    		}
+	    	}
+	    	return word.toString();
+		}
+		
+		public int getPercent() {
+			int filled = 0;
+			int empty = 0;
+			
+			for (int y = 0; y < this.height; y++)
+				for (int x = 0; x < this.width; x++)
+					if (this.area[y][x] != null) {
+						if (this.area[y][x].equals(" "))
+							empty++;
+						else
+							filled++;
+					}
+			return filled * 100 / (empty + filled);
 		}
 }
