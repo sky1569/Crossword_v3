42d3363247c6f0089e7bfda71696dbfb88dd8456
sky1569 <niyaotian@163.com>
2013-12-24 11:29:29 +0800

    ss

--- src/com/crossword/utils/MyComparator.java
+++ src/com/crossword/utils/MyComparator.java
@@ -1,6 +1,7 @@
 package com.crossword.utils;
-import java.util.*;
+import java.util.Comparator;
 
+import com.crossword.data.Grid;
 import com.crossword.data.Vol;
 
 public class MyComparator implements Comparator{
@@ -8,7 +9,7 @@ public class MyComparator implements Comparator{
 	@Override
 	public int compare(Object o1, Object o2) {
 		// TODO Auto-generated method stub
-		
+		if(o1 instanceof Vol){
 		Vol v1 = (Vol)o1;
 		Vol v2 = (Vol)o2;
 		
@@ -17,8 +18,20 @@ public class MyComparator implements Comparator{
 		}else{
 			return 1;
 		}
-	
+		}
+		else
+		{
+			Grid g1 =(Grid)o1;
+			Grid g2 =(Grid)o2;
+			
+			if(g1.getLevel() < g2.getLevel()){
+				return -1;
+			}else{
+				return 1;
+			}
+		}
 
 	}
 
+	
 }
