180b96798735a890192e02a91d3bbf74eae2fda1
qicaoyan111 <15210830240@139.com>
2014-02-19 10:26:31 +0800

    键盘做好了

--- src/com/crossword/view/MyGridView.java
+++ src/com/crossword/view/MyGridView.java
@@ -1,8 +1,6 @@
 package com.crossword.view;
 
 import com.crossword.Crossword;
-import com.crossword.view.InputTextView.MyInputConnection;
-
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
